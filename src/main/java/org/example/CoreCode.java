f37b408068f816d348abdca3abcf85ac5351d868
sanjeevh1 <sanjeevharikrishnan@gmail.com>
2025-05-18 16:25:23 -0700

    Added CoreCode enum

---
+++ src/main/java/org/example/CoreCode.java
@@ -0,0 +1,22 @@
+package org.example;
+
+/**
+ * Enum representing various core codes.
+ * Does not include core goals.
+ */
+public enum CoreCode {
+    AHo,
+    AHp,
+    AHq,
+    AHr,
+    CCD,
+    CCO,
+    HST,
+    NS,
+    QQ,
+    QR,
+    SCL,
+    WC,
+    WCd,
+    WCr
+}
