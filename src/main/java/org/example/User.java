47af355b9351e505b5015adc903ae64ed059430f
sanjeevh1 <sanjeevharikrishnan@gmail.com>
2025-05-30 19:51:31 -0700

    Created UserService

--- src/main/java/org/example/User.java
+++ src/main/java/org/example/User.java
@@ -17,4 +17,17 @@ public class User {
     @Column(nullable = false)
     private String password;
     private List<Course> courses;
+
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
---
+++ src/main/java/org/example/UserService.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    /**
+     * Signs up a new user with the provided username and password.
+     * @param username the username for the new user
+     * @param password the password for the new user
+     * @return the created User object
+     */
+    public User signUp(String username, String password) {
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(passwordEncoder.encode(password));
+        return userRepository.save(user);
+    }
+
+    /**
+     * Retrieves a user by their username.
+     * @param username the username of the user to retrieve
+     * @return the User object if found, otherwise throws an exception
+     */
+    public User getUserByUsername(String username) {
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+    }
+}
