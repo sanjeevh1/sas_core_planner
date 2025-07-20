f0b85356d1a6bc2251aa96851f0b575c52f6f6ae
sanjeevh1 <sanjeevharikrishnan@gmail.com>
2025-07-19 19:10:16 -0700

    Implemented AND logic in frontend

--- frontend/search.js
+++ frontend/search.js
@@ -2,6 +2,30 @@ const apiUrl = 'http://localhost:8080';
 if(localStorage.getItem('token') === null) {
     window.location.href = 'login.html';
 }
+andButton = document.getElementById('and-button');
+coresDiv = document.getElementById('cores');
+andButton.addEventListener('click', function() {
+    const andPara = document.createElement('p');
+    andPara.textContent = 'AND';
+    coresDiv.appendChild(andPara);
+    const coreSelect = document.createElement('select');
+    coreSelect.innerHTML = `
+        <option value="CCD">CCD</option>
+        <option value="CCO">CCO</option>
+        <option value="NS">NS</option>
+        <option value="SCL">SCL</option>
+        <option value="HST">HST</option>
+        <option value="AHo">AHo</option>
+        <option value="AHp">AHp</option>
+        <option value="AHq">AHq</option>
+        <option value="AHr">AHr</option>
+        <option value="WCr">WCr</option>
+        <option value="WCd">WCd</option>
+        <option value="WC">WC</option>
+        <option value="QQ">QQ</option>
+        <option value="QR">QR</option>`;
+    coresDiv.appendChild(coreSelect);
+});
 fetch(`${apiUrl}/user/courses`, {
     method: 'GET',
     headers: {
@@ -27,86 +51,85 @@ fetch(`${apiUrl}/user/courses`, {
             window.location.href = 'login.html';
         });
         searchButton.addEventListener('click', function() {
-            const core = coreField.value;
-            if (core) {
-                fetch(`${apiUrl}/courses/course-list`, {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify([ [core] ])
-                }).then(response => response.json())
-                 .then(courses => {
-                        courseList.innerHTML = ''; // Clear previous results
-                        fetch(`${apiUrl}/user/courses`, {
-                            method: 'GET',
-                            headers: {
-                                'Authorization': `Bearer ${localStorage.getItem('token')}`
-                            }
-                        }).then(response => {
-                            if (response.ok) {
-                                return response.json();
-                            } else {
-                                window.location.href = 'login.html';
-                            }
-                        }).then(takenCourses => {
-                            courses.forEach(course => {
-                                let courseDiv = document.createElement('div');
-                                let courseNumber = document.createElement('p');
-                                courseNumber.textContent = course.courseNumber;
-                                courseDiv.appendChild(courseNumber);
-                                let courseTitle = document.createElement('p');
-                                courseTitle.textContent = course.courseTitle;
-                                courseDiv.appendChild(courseTitle);
-                                let credits = document.createElement('p');
-                                credits.textContent = course.credits;
-                                courseDiv.appendChild(credits);
-                                let coreCodes = document.createElement('p');
-                                coreCodes.textContent = course.coreCodes.join(", ");
-                                courseDiv.appendChild(coreCodes);
-                                let subject = document.createElement('p');
-                                subject.textContent = course.subject;
-                                courseDiv.appendChild(subject);
-                                let addButton = document.createElement('button');
-                                addButton.textContent = takenCourses.some(c => c.id === course.id) ? '-' : '+';
-                                addButton.addEventListener('click', function() {
-                                    if( addButton.textContent === '+') {
-                                        fetch(`${apiUrl}/user/add/${course.id}`, {
-                                            method: 'POST',
-                                            headers: {
-                                                'Authorization': `Bearer ${localStorage.getItem('token')}`
-                                            }
-                                        }).then(response => {
-                                            if (response.ok) {
-                                                addButton.textContent = '-';
-                                            } else {
-                                                alert('Failed to add course.');
-                                            }
-                                        });
-                                    } else {
-                                        fetch(`${apiUrl}/user/remove/${course.id}`, {
-                                            method: 'DELETE',
-                                            headers: {
-                                                'Authorization': `Bearer ${localStorage.getItem('token')}`
-                                            }
-                                        }).then(response => {
-                                            if (response.ok) {
-                                                addButton.textContent = '+';
-                                            } else {
-                                                alert('Failed to remove course.');
-                                            }
-                                        });
-                                    }
-                                });
-                                courseDiv.appendChild(addButton);
-                                courseList.appendChild(courseDiv);
+            const cores = Array.from(coresDiv.querySelectorAll('select')).map(select => select.value);
+            fetch(`${apiUrl}/courses/course-list`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify([cores])
+            }).then(response => response.json())
+             .then(courses => {
+                    courseList.innerHTML = ''; // Clear previous results
+                    fetch(`${apiUrl}/user/courses`, {
+                        method: 'GET',
+                        headers: {
+                            'Authorization': `Bearer ${localStorage.getItem('token')}`
+                        }
+                    }).then(response => {
+                        if (response.ok) {
+                            return response.json();
+                        } else {
+                            window.location.href = 'login.html';
+                        }
+                    }).then(takenCourses => {
+                        courses.forEach(course => {
+                            let courseDiv = document.createElement('div');
+                            let courseNumber = document.createElement('p');
+                            courseNumber.textContent = course.courseNumber;
+                            courseDiv.appendChild(courseNumber);
+                            let courseTitle = document.createElement('p');
+                            courseTitle.textContent = course.courseTitle;
+                            courseDiv.appendChild(courseTitle);
+                            let credits = document.createElement('p');
+                            credits.textContent = course.credits;
+                            courseDiv.appendChild(credits);
+                            let coreCodes = document.createElement('p');
+                            coreCodes.textContent = course.coreCodes.join(", ");
+                            courseDiv.appendChild(coreCodes);
+                            let subject = document.createElement('p');
+                            subject.textContent = course.subject;
+                            courseDiv.appendChild(subject);
+                            let addButton = document.createElement('button');
+                            addButton.textContent = takenCourses.some(c => c.id === course.id) ? '-' : '+';
+                            addButton.addEventListener('click', function() {
+                                if( addButton.textContent === '+') {
+                                    fetch(`${apiUrl}/user/add/${course.id}`, {
+                                        method: 'POST',
+                                        headers: {
+                                            'Authorization': `Bearer ${localStorage.getItem('token')}`
+                                        }
+                                    }).then(response => {
+                                        if (response.ok) {
+                                            addButton.textContent = '-';
+                                        } else {
+                                            alert('Failed to add course.');
+                                        }
+                                    });
+                                } else {
+                                    fetch(`${apiUrl}/user/remove/${course.id}`, {
+                                        method: 'DELETE',
+                                        headers: {
+                                            'Authorization': `Bearer ${localStorage.getItem('token')}`
+                                        }
+                                    }).then(response => {
+                                        if (response.ok) {
+                                            addButton.textContent = '+';
+                                        } else {
+                                            alert('Failed to remove course.');
+                                        }
+                                    });
+                                }
                             });
-
+                            courseDiv.appendChild(addButton);
+                            courseList.appendChild(courseDiv);
                         });
-                 });
-            }
+
+                    });
+             });
         });
 });
 window.addEventListener('storage', function(event) {
     location.reload();
 });
+
