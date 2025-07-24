85bf8bfd9c289b3348327f4d8eff22844d572512
sanjeevh1 <sanjeevharikrishnan@gmail.com>
2025-07-24 12:51:47 -0700

    Refactored courses.js

--- frontend/courses.js
+++ frontend/courses.js
@@ -1,9 +1,6 @@
-const production = false
-//const apiUrl = production ? 'https://sas-core-h8a3hncpctaactgg.eastus-01.azurewebsites.net' : 'http://localhost:8080';
+//const apiUrl = 'http://localhost:8080';
 const apiUrl = 'https://sas-core-planner-latest.onrender.com'
-if(localStorage.getItem('token') === null) {
-    window.location.href = 'login.html';
-}
+
 const codesTaken = {
     "CCD": 0,
     "CCO": 0,
@@ -20,6 +17,7 @@ const codesTaken = {
     "QQ": 0,
     "QR": 0
 };
+
 const categoriesTaken = {
     "CCD/CCO": 0,
     "SCL/HST": 0,
@@ -27,73 +25,100 @@ const categoriesTaken = {
     "WCr/WCd": 0,
     "QQ/QR": 0
 };
+
+const courseTable = document.getElementById('course-table');
 let ahCodesTaken = 0;
 const ahCodes = document.getElementById("AH-codes");
-fetch(`${apiUrl}/user/courses`, {
-    method: 'GET',
-    headers: {
-        'Authorization': `Bearer ${localStorage.getItem('token')}`
-    }
-}).then(response => {
+
+function parseResponse(response) {
     if (response.ok) {
         return response.json();
     } else {
         window.location.href = 'login.html';
     }
-}).then(userCourses => {
-        const usernameField = document.getElementById('username');
-        const logoutButton = document.getElementById('logout');
-        usernameField.textContent = localStorage.getItem('username');
-        logoutButton.addEventListener('click', function() {
-            localStorage.removeItem('username');
-            localStorage.removeItem('token');
-            window.location.href = 'login.html';
-        });
-        const courseTable = document.getElementById('course-table');
-        userCourses.forEach(course => {
-            const row = courseTable.insertRow(-1);
-            row.id = `${course.courseNumber}-row`;
-            const numberCell = row.insertCell(0);
-            numberCell.textContent = course.courseNumber;
-            for(const code of Object.keys(codesTaken)) {
-                const cell = row.insertCell(-1);
-                if(course.coreCodes.includes(code)) {
-                    if(code.includes("AH") && codesTaken[code] === 0) {
-                        ahCodesTaken++;
-                        ahCodes.textContent = ahCodesTaken;
-                    }
-                    codesTaken[code]++;
-                    const codeSpan = document.getElementById(code);
-                    codeSpan.textContent = codesTaken[code];
-                    cell.style.backgroundColor = "green";
-                }
-            }
-            for(const category of Object.keys(categoriesTaken)) {
-                const codes = category.split("/");
-                if (codes.some(code => course.coreCodes.includes(code))) {
-                    categoriesTaken[category]++;
-                    const categorySpan = document.getElementById(category);
-                    categorySpan.textContent = categoriesTaken[category];
-                }
-            }
-            const removeCell = row.insertCell(-1);
-            const removeButton = document.createElement("button");
-            removeButton.classList.add("remove-btn");
-            removeButton.textContent = "-";
-            removeCell.appendChild(removeButton);
-            removeButton.addEventListener("click", () => {
-                fetch(`${apiUrl}/user/remove/${course.id}`, {
-                    method: 'DELETE',
-                    headers: {
-                        "Authorization": `Bearer ${localStorage.getItem('token')}`
-                    }
-                }).then(response => {
-                    location.reload();
-                }).catch(error => {
-                    window.location.href = 'login.html';
-                });
-            });
-        });
-}).catch(error => {
+}
+
+function addCores(row, cores) {
+    for(const code of Object.keys(codesTaken)) {
+       const cell = row.insertCell(-1);
+       if(cores.includes(code)) {
+           if(code.includes("AH") && codesTaken[code] === 0) {
+               ahCodesTaken++;
+               ahCodes.textContent = ahCodesTaken;
+           }
+           codesTaken[code]++;
+           const codeSpan = document.getElementById(code);
+           codeSpan.textContent = codesTaken[code];
+           cell.style.backgroundColor = "green";
+       }
+    }
+}
+
+function updateCategories(courseCores) {
+    for(const category of Object.keys(categoriesTaken)) {
+       const codes = category.split("/");
+       if (codes.some(code => courseCores.includes(code))) {
+           categoriesTaken[category]++;
+           const categorySpan = document.getElementById(category);
+           categorySpan.textContent = categoriesTaken[category];
+       }
+    }
+}
+
+function removeCourse(courseId) {
+    fetch(`${apiUrl}/user/remove/${courseId}`, {
+       method: 'DELETE',
+       headers: {
+           "Authorization": `Bearer ${localStorage.getItem('token')}`
+       }
+    }).then(response => {
+       location.reload();
+    }).catch(error => {
+       window.location.href = 'login.html';
+    });
+}
+
+function createRemoveButton(row, course) {
+    const removeCell = row.insertCell(-1);
+    const removeButton = document.createElement("button");
+    removeButton.classList.add("remove-btn");
+    removeButton.textContent = "-";
+    removeCell.appendChild(removeButton);
+    removeButton.addEventListener("click", () => {
+        removeCourse(course.id);
+    });
+}
+
+function addCourseRow(course) {
+    const row = courseTable.insertRow(-1);
+    const numberCell = row.insertCell(0);
+    numberCell.textContent = course.courseNumber;
+    addCores(row, course.coreCodes);
+    updateCategories(course.coreCodes);
+    createRemoveButton(row, course);
+}
+
+function logout() {
+    localStorage.removeItem('username');
+    localStorage.removeItem('token');
     window.location.href = 'login.html';
-});
+}
+
+function addCourses(userCourses) {
+     const usernameField = document.getElementById('username');
+     const logoutButton = document.getElementById('logout');
+     usernameField.textContent = localStorage.getItem('username');
+     logoutButton.addEventListener('click', logout);
+     const courseTable = document.getElementById('course-table');
+     userCourses.forEach(addCourseRow);
+}
+
+if(localStorage.getItem('token') === null) {
+    logout();
+}
+fetch(`${apiUrl}/user/courses`, {
+    method: 'GET',
+    headers: {
+        'Authorization': `Bearer ${localStorage.getItem('token')}`
+    }
+}).then(parseResponse).then(addCourses).catch(error => {logout();});
