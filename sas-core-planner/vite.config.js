62182cdd18fe12dde322d44d81b52ebfdf355cb6
sanjeevh1 <sanjeevharikrishnan@gmail.com>
2025-08-29 16:11:02 -0700

    Updated vite.config.js

--- sas-core-planner/vite.config.js
+++ sas-core-planner/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/sas_core_planner/"
+  base: "/sas_core_planner/sas-core-planner/"
 })
